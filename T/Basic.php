ed0337964403477984e0f6414ff7bcf2e1bd3047
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-04-23 10:02:35 +0300

    0.0.4

--- T/Basic.php
+++ T/Basic.php
@@ -1,8 +1,8 @@
 <?php
 namespace Dfe\Dynamics365\T;
-use Dfe\Dynamics365\Settings\General as sG;
+use Dfe\Dynamics365\Settings\General as S;
 // 2017-04-23
 final class Basic extends TestCase {
 	/** @test 2017-04-23 */
-	function t01() {echo sG::s()->apiKey();}
+	function t01() {echo S::s()->apiKey();}
 }
