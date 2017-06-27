d873a0cd149b58d8ac469383515739adb5cb9319
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-06-27 22:31:44 +0300

    no message

--- Controller/Oauth/Index.php
+++ Controller/Oauth/Index.php
@@ -1,6 +1,14 @@
 <?php
-namespace Dfe\Dynamics365\Controller\Oauth;
-// 2017-06-27
+/**
+ * 2017-06-27
+ * We can use «OAuth» namespace instead of «Oauth», because @see \Magento\Framework\App\Router\ActionList::get()
+ * uses @see strtolower for the controller names:
+ *	$fullPath = str_replace(
+ *		'_', '\\', strtolower($module . '\\controller' . $area . '\\' . $namespace . '\\' . $action)
+ *	);
+ * https://github.com/magento/magento2/blob/2.2.0-RC1.1/lib/internal/Magento/Framework/App/Router/ActionList.php#L100-L109
+ */
+namespace Dfe\Dynamics365\Controller\OAuth;
 /** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
 class Index extends \Df\OAuth\ReturnT {
 	/**
