55f19d0ebd504040dc1ae2e80fd63be4a47d6565
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-06-27 03:15:21 +0300

    no message

--- view/adminhtml/web/formElement/button/main.js
+++ view/adminhtml/web/formElement/button/main.js
@@ -1,13 +1,11 @@
 // 2017-06-27
-define(['jquery', 'Df_Core/ColorPicker', 'domReady!'], function($) {return (
+define(['jquery', 'domReady!'], function($) {return (
 	/**
 	 * 2017-06-27
 	 * @param {Object} config
 	 * @param {String} config.id
 	 */
-	function(config) {
-		/** https://github.com/bgrins/spectrum */
-		var $e = $(document.getElementById(config.id));
-		$e.click(function() {console.log('Authenticate!');});
-	}
+	function(config) {$(document.getElementById(config.id)).click(function() {
+		window.location = 'https://login.microsoftonline.com/common/oauth2/authorize?';
+	});}
 );});
