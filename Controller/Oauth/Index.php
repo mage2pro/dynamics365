0db70bb0f046066f8e7b6f3f9d08e5a0b69317f6
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-06-27 22:45:37 +0300

    no message

--- Controller/Oauth/Index.php
+++ Controller/Oauth/Index.php
@@ -9,12 +9,21 @@
  * https://github.com/magento/magento2/blob/2.2.0-RC1.1/lib/internal/Magento/Framework/App/Router/ActionList.php#L100-L109
  */
 namespace Dfe\Dynamics365\Controller\OAuth;
-/** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
+/**
+ * 2017-06-27
+ * @final Unable to use the PHP «final» keyword here because of the M2 code generation.
+ * We get a response like:
+ * 	{
+ * 		"code": <a string of 611 caracters>,
+ * 		"session_state": "4be3f979-ebcd-4846-9898-6cf007756456"
+ * 	}
+ */
 class Index extends \Df\OAuth\ReturnT {
 	/**
 	 * 2017-06-27
 	 * @override
 	 * @see \Df\OAuth\ReturnT::_execute()
+	 * @used-by \Df\OAuth\ReturnT::execute()
 	 */
-	final protected function _execute() {}
+	final protected function _execute() {df_log_l($this, $_REQUEST);}
 }
