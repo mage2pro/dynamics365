4ad7efe9e1bc48bc5871bb8bb4196d485576da7c
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-06-27 22:14:14 +0300

    0.0.6

---
+++ Controller/Oauth/Index.php
@@ -0,0 +1,12 @@
+<?php
+namespace Dfe\Dynamics365\Controller\Oauth;
+// 2017-06-27
+/** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
+class Index extends \Df\OAuth\ReturnT {
+	/**
+	 * 2017-06-27
+	 * @override
+	 * @see \Df\OAuth\ReturnT::_execute()
+	 */
+	final protected function _execute() {}
+}
