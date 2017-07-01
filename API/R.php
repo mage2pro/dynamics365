8dc58b20380ac7eca2a20d08340e358d69dd934c
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-07-01 04:03:35 +0300

    0.1.4

--- API/R.php
+++ API/R.php
@@ -7,10 +7,25 @@ use Zend_Http_Client as C;
 final class R {
 	/**
 	 * 2017-06-30
+	 * @return array(string => mixed)
+	 */
+	static function accounts() {return self::p(__FUNCTION__);}
+
+	/**
+	 * 2017-06-30
+	 * @return array(string => mixed)
+	 */
+	static function service() {return self::p('');}
+
+	/**
+	 * 2017-06-30
+	 * @used-by accounts()
+	 * @used-by service()
 	 * @param string $path
 	 * @param string $method [optional]
 	 * @param array(string => mixed) $p [optional]
 	 * @return array(string => mixed)
+	 * @throws DFE
 	 */
 	static function p($path, $method = C::GET, array $p = []) {
 		/** @var C $c */
@@ -23,9 +38,46 @@ final class R {
 				,'OData-Version' => '4.0'
 			])
 			->setMethod($method)
-			->setUri(S::s()->url() . $path)
+			->setUri(df_cc_path(S::s()->url(), 'api/data/v8.2', $path))
 		;
 		C::GET === $method ? $c->setParameterGet($p) : $c->setParameterPost($p);
-		return df_json_decode($c->request()->getBody());
+		/** @var array(string => mixed) $r */
+		$r = df_json_decode($c->request()->getBody());
+		/**
+		 * 2017-06-30
+		 * An error response looks like:
+		 *	{
+		 *		"error": {
+		 *			"code": "",
+		 *			"message": "Resource not found for the segment 'dummy'.",
+		 *			"innererror": {
+		 *				"message": "Resource not found for the segment 'dummy'.",
+		 *				"type": "Microsoft.OData.Core.UriParser.ODataUnrecognizedPathException",
+		 *				"stacktrace": "<...>"
+		 *			}
+		 *		}
+		 *	}
+		 * @var array(string => string|array(string => string))|null $e
+		 */
+		if ($e = dfa($r, 'error')) {
+			// 2017-06-30 It correctly works even if the key does not exist in the array.
+			unset($e['innererror']['stacktrace']);
+			/** @var string $message */
+			$message = dfa($e, 'message');
+			/** @var string $req */
+			$req = df_zf_http_last_req($c);
+			/** @var string $res */
+			$res = df_json_encode_pretty($e);
+			/** @var DFE $ex */
+			$ex = df_error_create(
+				"The «{$path}» Dynamics 365 Web API request has failed: «{$message}»."
+				."\nThe full error description:\n$res"
+				."\nThe full request:\n$req"
+			);
+			df_log_l(__CLASS__, $ex);
+			df_sentry(__CLASS__, $message, ['extra' => ['Request' => $req, 'Response' => $res]]);
+			throw $ex;
+		}
+		return $r;
 	}
 }
--- T/Basic.php
+++ T/Basic.php
@@ -6,6 +6,9 @@ final class Basic extends TestCase {
 	/** 2017-04-23 */
 	function t00() {}
 
+	/** 2017-06-30 */
+	function t01() {echo df_dump(df_json_encode_pretty(R::service()));}
+
 	/** @test 2017-06-30 */
-	function t01() {echo df_dump(R::p('/api/data/v8.2/accounts'));}
+	function t02_invalid() {echo df_dump(df_json_encode_pretty(R::p('dummy')));}
 }
