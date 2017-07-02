9f3c04ec2412f592fe3a97d2b03e046a72cb359c
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-07-03 00:56:58 +0300

    no message

--- API/Client.php
+++ API/Client.php
@@ -53,6 +53,23 @@ abstract class Client {
 			// https://msdn.microsoft.com/en-us/library/gg334391.aspx#bkmk_headers
 			,'OData-MaxVersion' => '4.0'
 			,'OData-Version' => '4.0'
+			/**
+			 * 2017-07-03
+			 * «Include formatted values»:
+			 * https://msdn.microsoft.com/en-us/library/gg334767.aspx#bkmk_includeFormattedValues
+			 * A piece of a not-formatted response:
+			 *	"_modifiedby_value": "6e0e07b6-1aec-415e-acef-1f27907ea64d",
+			 *	"_organizationid_value": "d4932579-c761-493f-9ecd-992067fc4035",
+			 *	"_transactioncurrencyid_value": "fa42038c-da55-e711-810b-c4346bdcf161"
+			 * The same piece formatted:
+			 *	"_modifiedby_value": "6e0e07b6-1aec-415e-acef-1f27907ea64d",
+			 * 	"_modifiedby_value@OData.Community.Display.V1.FormattedValue": "Dmitry Fedyuk",
+			 *	"_organizationid_value": "d4932579-c761-493f-9ecd-992067fc4035",
+			 *	"_organizationid_value@OData.Community.Display.V1.FormattedValue": "mage20",
+			 *	"_transactioncurrencyid_value": "fa42038c-da55-e711-810b-c4346bdcf161",
+			 *	"_transactioncurrencyid_value@OData.Community.Display.V1.FormattedValue": "US Dollar",
+			 */
+			,'Prefer' => 'odata.include-annotations="OData.Community.Display.V1.FormattedValue"'
 			,'User-Agent' => sprintf('The «Dynamics 365» extension for Magento 2 (https://mage2.pro/c/extensions/dynamics365) used on the %s website', df_domain_current())
 		]);
 		$c->setUri(df_cc_path(S::s()->url(), 'api/data/v8.2', $this->path()));
