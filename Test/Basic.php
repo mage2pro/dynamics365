234b58b4ff77fa1e7fef1c29eda25f9c540ebbd0
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-24 04:35:34 +0000

    https://github.com/mage2pro/core/issues/168

--- Test/Basic.php
+++ Test/Basic.php
@@ -4,22 +4,22 @@ use Dfe\Dynamics365\API\Facade as F;
 # 2017-04-23
 final class Basic extends TestCase {
 	/** 2017-04-23 @test */
-	function t00() {}
+	function t00():void {}
 
 	/** 2017-07-01 */
-	function accounts() {$this->p();}
+	function accounts():void {$this->p();}
 
 	/** 2017-06-30 */
-	function invalid() {$this->p('dummy');}
+	function invalid():void {$this->p('dummy');}
 
 	/** 2017-04-23 */
-	function metadata() {print_r(F::metadata());}
+	function metadata():void {print_r(F::metadata());}
 
 	/** 2017-07-01 */
-	function products() {$this->o(array_filter(F::products(), function(array $p) {return
+	function products():void {$this->o(array_filter(F::products(), function(array $p) {return
 		df_starts_with($p['name'], 'Stripe')
 	;}));}
 
 	/** 2017-06-30 */
-	function service() {$this->o((F::service()));}
+	function service():void {$this->o((F::service()));}
 }
